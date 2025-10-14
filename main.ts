74308a4bc71e33ad446a51367ce89856debd2cdd
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-14 16:37:39 +0000

    /** *@author mr loosli *@version 1.0.0 *@date 2025-10-14*@ fileoverview A simple program that greets users by name. */
    // print out the greeting
    console.log(Hello,World!)

---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+/** 
+*@author mr loosli
+*@version 1.0.0
+*@date 2025-10-14\
+*@ fileoverview A simple program that greets a user by name.
+*/
+
+// print out the greeting 
+console.log("Hello, World!");
+
+console.log("\nDone.");
+
