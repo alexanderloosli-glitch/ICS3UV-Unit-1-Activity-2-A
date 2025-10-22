60aa6ab07173f667201df7a402bf961e136200b9
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-22 15:48:16 +0000

    added to file pachage main, import fmt, fmt.Println

---
+++ main.go
@@ -0,0 +1,17 @@
+/*
+ * Author: Mr Loosli
+ * Version 1.0.0
+ * Date 2025-10-22
+ * This is the traditionall "Hello, World!" program.
+*/
+
+package main
+
+import "fmt"
+
+func main () {
+    // print out the greeting 
+		fmt.Println("Hello, World!")
+
+		fmt.Println("\nDone.")
+} 
---
+++ main.ts
@@ -0,0 +1,14 @@
+/**
+* @author alexander Loosli
+* @version 1.0.0
+* @date 2025-10-20
+* @fileoverview This program dispays my first and last name 
+*/
+
+// print out the name 
+console.log("Hello, World!");
+
+console.log("alexander");
+console.log("loosli");
+
+console.log("\nDone.");
